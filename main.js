60928dd2287fe10e81fe92c87065bb1afbc3c9a2
mustafa <mustafa.naouf@outlook.com>
2022-08-24 14:14:23 +0200

    resolved merge conflict for my recent changes

--- main.js
+++ main.js
@@ -38,15 +38,29 @@
 // console.log(body.background)
 // })
 const btn = document.getElementById("nightModeButton");
+const btnIcon = document.getElementById("iconTest");
 const container = document.querySelector(".container");
     btn.addEventListener("click", function onClick(event) {
-    if (container.style.background !== "blanchedalmond") {
+	btnIcon.classList.toggle("fa-moon");
+	btnIcon.classList.toggle("fa-sun");
+    if (container.classstyle.background !== "blanchedalmond") {
         container.style.background = "blanchedalmond";
     } else {
             container.style.background = "gray";
     }
 
 });
+
+// const btn = document.getElementById("nightModeButton");
+// const btnIcon = document.getElementById("iconTest");
+// const container = document.querySelector(".container");
+//     btn.addEventListener("click", function onClick(event) {
+// 	btnIcon.classList.toggle("fa-moon");
+// 	btnIcon.classList.toggle("fa-sun");
+//     container.classList.toggle('containerLight');
+//     container.classList.toggle('containerDark');
+
+// });
 
 // const button2 = document.querySelector(".bt");
 // button2.addEventListener("click", function onClick(event){
